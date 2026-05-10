e8c96722207c3e14135871f330a9bc48a06033f6
Jan Kotanski <jankotan@gmail.com>
2026-05-10 22:21:27 +0200

    Issue 105: add ubuntu  26.04 tests (#106)
    * add ubuntu  26.04 tests
    
    * add ubuntu  26.04 tests
    
    * add ubuntu  26.04 tests
    
    * ignore error 139

--- .ci/run.sh
+++ .ci/run.sh
@@ -5,5 +5,10 @@ if [ "$2" = "2" ]; then
 else
     echo "run python3-nxsconfigserver tests"
     docker exec ndts python3 -m pytest
-fi    
-if [ "$?" != "0" ]; then exit -1; fi
+fi
+ERR=$?
+if [ $ERR != 0 ]; then
+    if [ $ERR != 139 ]; then
+	exit $ERR;
+    fi
+fi

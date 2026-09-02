a677c05f238982210bed35a0a2d86999d30776b9
Jan Kotanski <jankotan@gmail.com>
2026-09-02 09:44:40 +0200

    add pixi tests

--- .ci/run_pixi.sh
+++ .ci/run_pixi.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
 echo "install pixi"
-docker exec  ndts /bin/bash -c 'curl -fsSL https://pixi.sh/install.sh | sh ; export PATH=$HOME/.pixi/bin:$PATH ; pixi shell-hook  --manifest-path .github/workflows/pixi/pixi.toml > .sh.sh ; source .sh.sh ; pixi add  --manifest-path .github/workflows/pixi/pixi.toml rattler-build '
+docker exec  ndts /bin/bash -c 'curl -fsSL https://pixi.sh/install.sh | sh ; export PATH=/var/lib/tango/.pixi/bin:$PATH ; pixi shell-hook  --manifest-path .github/workflows/pixi/pixi.toml > .sh.sh ; source .sh.sh ; pixi add  --manifest-path .github/workflows/pixi/pixi.toml rattler-build nxsconfigserver-db'
+# /home/tango
 docker exec  ndts /bin/bash -c 'pwd '
+# /var/lib/tango
 docker exec  ndts /bin/bash -c 'echo "$HOME" '
+
+# create nxsconfig database
+docker exec  --user root ndts /bin/bash -c 'source /var/lib/tango/.sh.sh ; /var/lib/tango/create_nxsconfig_db -d=nxsonfig -u=tanngo -p="$CONDA_PREFIX"'
 
 echo "run nxsconfigserver-db"
 docker exec  ndts /bin/bash -c 'source .sh.sh ; pixi run  --manifest-path .github/workflows/pixi/pixi.toml rattler-build build  --recipe .github/workflows/pixi/recipe.yaml'

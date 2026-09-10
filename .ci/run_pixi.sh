6668b846fe66bee7369b3cbeccdf5bc372aa0406
Jan Kotanski <jankotan@gmail.com>
2026-09-10 08:35:45 +0200

    Issue 119: simplify the pixi tests (#120)
    * simplify the tests
    
    * update python version
    
    * fix typo

--- .ci/run_pixi.sh
+++ .ci/run_pixi.sh
@@ -1,18 +1,14 @@
 #!/usr/bin/env bash
 
+
 echo "install pixi"
-docker exec  ndts /bin/bash -c 'curl -fsSL https://pixi.sh/install.sh | sh ; export PATH=/var/lib/tango/.pixi/bin:$PATH ; pixi shell-hook  --manifest-path .github/workflows/pixi/pixi.toml > .sh.sh ; source .sh.sh ; pixi add  --manifest-path .github/workflows/pixi/pixi.toml rattler-build'
-# docker exec  ndts /bin/bash -c 'curl -fsSL https://pixi.sh/install.sh | sh ; export PATH=/var/lib/tango/.pixi/bin:$PATH ; pixi shell-hook  --manifest-path .github/workflows/pixi/pixi.toml > .sh.sh ; source .sh.sh ; pixi add  --manifest-path .github/workflows/pixi/pixi.toml rattler-build nxsconfigserver-db'
-# # /home/tango
-# docker exec  ndts /bin/bash -c 'pwd '
-# # /var/lib/tango
-# docker exec  ndts /bin/bash -c 'echo "$HOME" '
+docker exec  ndts /bin/bash -c 'curl -fsSL https://pixi.sh/install.sh | sh ; export PATH=/var/lib/tango/.pixi/bin:$PATH ; cp .github/workflows/pixi/pixi.toml . ; pixi shell-hook  > .sh.sh ; source .sh.sh ; pixi add   numpy "pytango=10.1.4" setuptools pip wheel argcomplete lxml pytz pyyaml  python-dateutil pninexus fabio h5py matplotlib-base blissdata pytest docutils pymysql'
 
 # # create nxsconfig database
 # docker exec  --user root ndts /bin/bash -c 'source /home/tango/.sh.sh ; export MYSQL_PASSWORD="rootpw" ; create_nxsconfig_db -x -d=nxsonfig -u=tango -p="$CONDA_PREFIX"'
 
-echo "run nxsconfigserver-db"
-docker exec  ndts /bin/bash -c 'source .sh.sh ; pixi run  --manifest-path .github/workflows/pixi/pixi.toml rattler-build build  --recipe .github/workflows/pixi/recipe.yaml'
+echo "run nxsconfigserver tests"
+docker exec  ndts /bin/bash -c 'source .sh.sh ;  echo "export MYTANGO_PREFIX=$CONDA_PREFIX/bin" > /home/tango/.env ;   python -m pip install . -vv --no-deps --no-build-isolation ;   python -m pytest test'
 
 ERROR=$?
 if [ $ERROR -ne "0" ]
